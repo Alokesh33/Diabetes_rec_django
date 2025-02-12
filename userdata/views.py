e0fe1a87a46cba25c0a83c8b436a599d1a1ad3dc
Alokesh33 <alokeshmahato4@gmail.com>
2025-02-12 21:44:56 +0530

    Update views.py

--- userdata/views.py
+++ userdata/views.py
@@ -16,7 +16,7 @@ def predict(request):
     return render(request,'predict.html')
 
 def result(request):
-    data=pd.read_csv(r'D:\\download\\archive (3)\\diabetes.csv')
+    data=pd.read_csv(r'/home/ubuntu/data/diabetes.csv')
 
     X=data.drop("Outcome", axis=1)
     Y=data["Outcome"]
